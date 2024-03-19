d31b6a493e1b5a3c1c9994f154426d1182f3f4b6
Josh Seligman <joshuaseligman18@gmail.com>
2024-03-19 15:44:29 -0400

    Got basic lab 3b working

--- lab3b/lab3b.ino
+++ lab3b/lab3b.ino
@@ -12,25 +12,25 @@ const int HEAD_SERVO_PIN = 22;
 const int ECHO_PIN = 4;
 const int TRIG_PIN = 5;
 
-// US variables
 const float MAX_DISTANCE = 300.0f;
 
-const unsigned long PERIOD = 15;
-unsigned long prevMillis = 0;
+const unsigned long PID_PERIOD = 15;
+unsigned long prevPidMillis = 0;
 
 const float TARGET = 30.48f;
 
 const bool P_ON = true;
 const bool I_ON = true;
 const bool D_ON = true;
+const bool HEAD_ON = true;
 
 const float K_P = 2.85f;
-const float K_I = 0.05f;
-const float K_D = 0.35f;
+const float K_I = 0.035f;
+const float K_D = 2.85f;
 
-const int BASE_SPEED = 75;
-const int MIN_SPEED = 50;
-const int MAX_SPEED = 100;
+const int BASE_SPEED = 100;
+const int MIN_SPEED = 75;
+const int MAX_SPEED = 125;
 const int HALF_SPEED_RANGE = (MAX_SPEED - MIN_SPEED) / 2;
 
 const int PID_THRESHOLD = 5;
@@ -40,19 +40,26 @@ const float PID_RANGE = PID_MAX;
 
 float prevError = 0.0f;
 float errorSum = 0.0f;
-const float INTEGRAL_THRESHOLD = 12.0f;
+const float INTEGRAL_THRESHOLD = 10.0f;
+
+const unsigned long HEAD_PERIOD = 750;
+unsigned long prevHeadMillis = 0;
+const int SIDE_POSITION = 170;
+const int FRONT_POSITION = 90;
+const float TARGET_FRONT = 30.48f;
+const float K_P_FRONT = 4.25f;
+float pidAdjustment = 0;
 
 void setup() {
     Serial.begin(57600);
 
     // Set up servo pins
     headServo.attach(HEAD_SERVO_PIN);
-    headServo.write(170);
+    headServo.write(SIDE_POSITION);
     
     // Set up US pins
     pinMode(ECHO_PIN, INPUT);
     pinMode(TRIG_PIN, OUTPUT);
-
 
     delay(1000);
     buzzer.play("c32");
@@ -60,10 +67,14 @@ void setup() {
 
 void loop() {
     unsigned long curMillis = millis();
-    if (curMillis >= prevMillis + PERIOD) {
+    if (curMillis >= prevPidMillis + PID_PERIOD) {
         float distance = usReadCm();
-        runPid(distance);
-        prevMillis = curMillis;
+        float pidCalculation = runPid(distance);
+        if (HEAD_ON) {
+            checkFront();
+        }
+        adjustMotors(pidCalculation + pidAdjustment);
+        prevPidMillis = curMillis;
     }
 }
 
@@ -90,7 +101,7 @@ float usReadCm() {
     return distance;
 }
 
-void runPid(float curDistance) {
+float runPid(float curDistance) {
     float error = curDistance - TARGET;
 
     float pidCalculation = 0;
@@ -112,7 +123,7 @@ void runPid(float curDistance) {
 
     if (D_ON) {
         float dErr = error - prevError;
-        pidCalculation += K_P * dErr;
+        pidCalculation += K_D * dErr;
         prevError = error;
     }
 
@@ -122,7 +133,37 @@ void runPid(float curDistance) {
         pidCalculation = PID_MAX;
     }
 
-    float normalizedPid = pidCalculation / PID_RANGE * HALF_SPEED_RANGE;
+    return pidCalculation;
+}
+
+void checkFront() {
+    float curMillis = millis();
+    if (curMillis >= prevHeadMillis + HEAD_PERIOD) {
+        headServo.write(FRONT_POSITION);
+        delay(200);
+
+        float distanceAhead = usReadCm();
+        // Serial.println(distanceAhead);
+        float error = distanceAhead - TARGET_FRONT;
+        if (error < 0) {
+            error *= -1;
+        } else if (error > TARGET_FRONT) {
+            error = TARGET_FRONT;
+        }
+        // We want the inverse error, so closer to the target means more action
+        error = TARGET_FRONT - error;
+        Serial.println(error);
+        
+        pidAdjustment = -1 * K_P_FRONT * error;        
+
+        headServo.write(SIDE_POSITION);
+        delay(200);
+        prevHeadMillis = curMillis;
+    }
+}
+
+void adjustMotors(float finalPidCalculation) {
+    float normalizedPid = finalPidCalculation / PID_RANGE * HALF_SPEED_RANGE;
 
     int rightSpeed = (int)(BASE_SPEED + normalizedPid);
     if (rightSpeed < MIN_SPEED) {
